86de5a8d033809969d484eb749b3a1ea08024d4b
Jafar Khoshtabiat <khoshtabiat.jafar@gmail.com>
2023-05-25 10:15:41 +0330

    add colors as class properties of UIColor

---
+++ Refah/extension+UIColor.swift
@@ -0,0 +1,17 @@
+//
+//  extension+UIColor.swift
+//  Refah
+//
+//  Created by Jafar Khoshtabiat on 5/25/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static let extinctVolcano = UIColor(named: "ExtinctVolcano")
+    static let flameHawkfish = UIColor(named: "FlameHawkfish")
+    static let fuelTown = UIColor(named: "FuelTown")
+    static let herbal = UIColor(named: "Herbal")
+    static let juneberry = UIColor(named: "Juneberry")
+    static let unicornSilver = UIColor(named: "UnicornSilver")
+}
