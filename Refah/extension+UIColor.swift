8613f877027afac9a1a292272e9213492c0f9145
Jafar Khoshtabiat <khoshtabiat.jafar@gmail.com>
2023-05-27 00:59:09 +0330

    add DeepMagenta and ShadowMountain colors

--- Refah/extension+UIColor.swift
+++ Refah/extension+UIColor.swift
@@ -10,11 +10,13 @@ import UIKit
 extension UIColor {
     static let antiFlashWhite = UIColor(named: "AntiFlashWhite")
     static let carnival = UIColor(named: "Carnival")
+    static let deepMagenta = UIColor(named: "DeepMagenta")
     static let deepSeaDream = UIColor(named: "DeepSeaDream")
-    static let extinctVolcano = UIColor(named: "ExtinctVolcano")
+    static let extinctVolcano = UIColor(named: "ExtinctVolcano")    // #49505A
     static let flameHawkfish = UIColor(named: "FlameHawkfish")
     static let fuelTown = UIColor(named: "FuelTown")
     static let herbal = UIColor(named: "Herbal")
     static let juneberry = UIColor(named: "Juneberry")
+    static let shadowMountain = UIColor(named: "ShadowMountain")
     static let unicornSilver = UIColor(named: "UnicornSilver")
 }
