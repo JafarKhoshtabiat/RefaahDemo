4544aa33416d400e68a7771d79bf5d79cbd9f46d
Jafar Khoshtabiat <khoshtabiat.jafar@gmail.com>
2023-06-07 14:12:26 +0330

    add ConfigurationConstants to store constants related to application

---
+++ Refah/ConfigurationConstants.swift
@@ -0,0 +1,12 @@
+//
+//  ConfigurationConstants.swift
+//  Refah
+//
+//  Created by Jafar Khoshtabiat on 6/5/23.
+//
+
+import Foundation
+
+struct ConfigurationConstants {
+    static let baseURL = "http://localhost:8080"
+}
