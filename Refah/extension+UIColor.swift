252a7fb035959a8a8a069ae1e407b1b0cbdb7235
Jafar Khoshtabiat <khoshtabiat.jafar@gmail.com>
2023-06-07 14:11:30 +0330

    force unwrap custom static colors in UIColor

--- Refah/extension+UIColor.swift
+++ Refah/extension+UIColor.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 extension UIColor {
-    static let antiFlashWhite = UIColor(named: "AntiFlashWhite")
-    static let carnival = UIColor(named: "Carnival")
-    static let deepMagenta = UIColor(named: "DeepMagenta")
-    static let deepSeaDream = UIColor(named: "DeepSeaDream")
-    static let extinctVolcano = UIColor(named: "ExtinctVolcano")    // #49505A
-    static let flameHawkfish = UIColor(named: "FlameHawkfish")
-    static let fuelTown = UIColor(named: "FuelTown")
-    static let herbal = UIColor(named: "Herbal")
-    static let juneberry = UIColor(named: "Juneberry")
-    static let shadowMountain = UIColor(named: "ShadowMountain")    // #585858 (r: 88, g: 88, b: 88)
-    static let unicornSilver = UIColor(named: "UnicornSilver")
+    static let antiFlashWhite = UIColor(named: "AntiFlashWhite")!
+    static let carnival = UIColor(named: "Carnival")!
+    static let deepMagenta = UIColor(named: "DeepMagenta")!
+    static let deepSeaDream = UIColor(named: "DeepSeaDream")!
+    static let extinctVolcano = UIColor(named: "ExtinctVolcano")!    // #49505A
+    static let flameHawkfish = UIColor(named: "FlameHawkfish")!
+    static let fuelTown = UIColor(named: "FuelTown")!
+    static let herbal = UIColor(named: "Herbal")!
+    static let juneberry = UIColor(named: "Juneberry")!
+    static let shadowMountain = UIColor(named: "ShadowMountain")!    // #585858 (r: 88, g: 88, b: 88)
+    static let unicornSilver = UIColor(named: "UnicornSilver")!
 }
