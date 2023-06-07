145a275ee8eeedbea93acfa86abfcadd5fd933cc
Jafar Khoshtabiat <khoshtabiat.jafar@gmail.com>
2023-06-08 02:02:57 +0330

    change baseurl to hetzner server ip

--- Refah/ConfigurationConstants.swift
+++ Refah/ConfigurationConstants.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 struct ConfigurationConstants {
-    static let baseURL = "http://localhost:8080"
+//    static let baseURL = "http://localhost:8080"
+    static let baseURL = "http://65.109.202.174:8080"
 }
