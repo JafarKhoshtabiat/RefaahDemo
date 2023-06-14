d2134e55aa4377bb65518f80a79796cee4836a75
Jafar Khoshtabiat <khoshtabiat.jafar@gmail.com>
2023-06-14 05:06:48 +0330

    add BlueAnthracite, Jupiter, KingCreekFalls and LavendarWisp colors

--- Refah/extension+UIColor.swift
+++ Refah/extension+UIColor.swift
@@ -9,6 +9,7 @@ import UIKit
 
 extension UIColor {
     static let antiFlashWhite = UIColor(named: "AntiFlashWhite")!
+    static let blueAnthracite = UIColor(named: "BlueAnthracite")!    // #545C66 (r: 84, g: 92, b: 102)
     static let carnival = UIColor(named: "Carnival")!
     static let deepMagenta = UIColor(named: "DeepMagenta")!
     static let deepSeaDream = UIColor(named: "DeepSeaDream")!
@@ -17,6 +18,9 @@ extension UIColor {
     static let fuelTown = UIColor(named: "FuelTown")!
     static let herbal = UIColor(named: "Herbal")!
     static let juneberry = UIColor(named: "Juneberry")!
+    static let jupiter = UIColor(named: "Jupiter")!                  // #E1E1E1 (r: 225, g: 225, b: 225)
+    static let kingCreekFalls = UIColor(named: "KingCreekFalls")!    // #60666E (r: 96, g: 102, b: 110)
+    static let lavendarWisp = UIColor(named: "LavendarWisp")!        // #EAEBEE (r: 234, g: 235, b: 238)
     static let shadowMountain = UIColor(named: "ShadowMountain")!    // #585858 (r: 88, g: 88, b: 88)
     static let unicornSilver = UIColor(named: "UnicornSilver")!
 }
